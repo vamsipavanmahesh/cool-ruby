90bb011363a55e77c0a6a63f79b2ee1a0c6f9b13
vamsi <vamsi@quintype.com>
2019-07-27 23:43:51 +0530

    stategy pattern

---
+++ strategy_pattern.rb
@@ -0,0 +1,48 @@
+class Report
+  def self.data(formatter = JsonFormat)
+    data = "foobar" # assume that we got it after querying DB
+    formatter.format("foobar")
+  end
+end
+
+# Now I have a class called Report, which has data as an instance method
+# Now I would like to get the report in the json and html format
+# without actually wanting to hard code the information in the Report class
+
+
+
+class JsonFormatter
+  def self.format(data)
+    { 'foo' => data }
+  end
+end
+
+class HtmlFormatter
+  def self.format(data)
+    <h1> data </h1>
+  end
+end
+
+
+
+# Let's say controller gets a request for data to be presnt in json format
+# we find the format and see if we have a format
+# 
+
+class ReportsController
+
+  FORMATTERS = {
+    'json' => JsonFormatter
+    'html' => HtmlFormatter
+  }
+
+  def show
+    Report.data(formatter)
+  end
+
+  private
+
+  def formatter
+    FORMATTERS.fetch(params['format'])
+  end 
+end
